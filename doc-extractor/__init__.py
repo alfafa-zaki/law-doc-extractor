faf53419f1e1f68d7340991a6fcd5ecabf0b1d74
Alfafa <alfafa26zaki@gmail.com>
2025-02-17 10:58:10 +0700

    update __init__.py

--- doc-extractor/__init__.py
+++ doc-extractor/__init__.py
@@ -1,3 +1,32 @@
-from docExtract.processor import PDFProcessor, PDFStructure, json
+# pdf_processor/__init__.py
 
-__all__ = ["PDFProcessor", "PDFStructure"]
+from .processor import PDFProcessor, PDFStructure
+from .constants import (
+    POPPLER_PATH,
+    TESSERACT_CMD,
+    SK_PATTERN,
+    TITLE_PATTERN,
+    MENIMBANG_PATTERN,
+    BAB_PATTERN,
+    PASAL_PATTERN,
+    AYAT_PATTERN,
+    CLOSING_PATTERN,
+    LEGITIMATION_PATTERN,
+    MENGINGAT_PATTERN,
+)
+
+__all__ = [
+    "PDFProcessor",
+    "PDFStructure",
+    "POPPLER_PATH",
+    "TESSERACT_CMD",
+    "SK_PATTERN",
+    "TITLE_PATTERN",
+    "MENIMBANG_PATTERN",
+    "BAB_PATTERN",
+    "PASAL_PATTERN",
+    "AYAT_PATTERN",
+    "CLOSING_PATTERN",
+    "LEGITIMATION_PATTERN",
+    "MENGINGAT_PATTERN",
+]
