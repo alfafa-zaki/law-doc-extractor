4dfd82f1a3f621a1ef42005141902ac17c4d6675
Alfafa <alfafa26zaki@gmail.com>
2025-02-17 17:00:02 +0700

    create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="LawDocExtractor",
+    version="0.1.0",
+    author="Alfafa Zaki",
+    author_email="alfafa26zaki@gmail.com",
+    description="A library to process and extract structured data from law (Peraturan Presiden) PDF document.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alfafa-zaki/law-doc-extractor",
+    packages=find_packages(),
+    install_requires=[
+        "pypdf>=3.0.0",
+        "pytesseract>=0.3.10",
+        "pdf2image>=1.16.3",
+        "pydantic>=2.0.0",
+        "Pillow>=10.0.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
