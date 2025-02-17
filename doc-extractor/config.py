c4ed3082fe8f5ae0703b5af61f63cc968c58f277
Alfafa <alfafa26zaki@gmail.com>
2025-02-17 10:49:50 +0700

    update config.py
    add constant variables and requirement path

--- doc-extractor/config.py
+++ doc-extractor/config.py
@@ -1,1 +1,11 @@
-
+POPPLER_PATH = r"poppler-24.08.0\Library\bin"
+TESSERACT_CMD = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+SK_PATTERN = r'SK No\s?\d+[A-Z]?'
+TITLE_PATTERN = r"PERATURAN PRESIDEN REPUBLIK INDONESIA"
+MENIMBANG_PATTERN = r"bahwa"
+BAB_PATTERN = r"(BAB [IVXLCDM]+\s+[A-Z ]+)\n"
+PASAL_PATTERN = r"\n(Pasal \d+)\n"
+AYAT_PATTERN = r"\(\s*\d+\s*\)"
+CLOSING_PATTERN = r"KETENTUAN PENUTUP"
+LEGITIMATION_PATTERN = r"Ditetapkan di"
+MENGINGAT_PATTERN = r"Mengingat\s*:(.*?)(?=Menetapkan|MEMUTUSKAN|$)"
